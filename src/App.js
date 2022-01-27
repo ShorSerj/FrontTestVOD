67fc59bc16115982cccf08e001f0c97b668b8930
ShorSerj <shorser8@gmail.com>
2022-01-27 20:41:22 +0300

    add banner and correct webpack(add Copy Plugin)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/reset.css'
+import Banner from './components/Banner/Banner.jsx';
+
+
+function App(prop) {
+  return (
+    <Banner/>
+  )
+}
+
+export default App
+
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Banner.module.scss'
+
+function Banner(prop) {
+  return (
+    <div className={s.baner}>
+        <div className={s.title}>
+            <h3>Исполните мечту вашего малыша!</h3>
+            <h3>Подарите ему собаку!</h3>
+        </div>
+        <img src="/img/qr-code.png" alt="" />
+        <p className={s.baner__instruction}>Сканируйте QR-код или нажмите ОК</p>
+      <button className={s.baner__button}>OK</button>
+    </div>
+  )
+}
+
+export default Banner
