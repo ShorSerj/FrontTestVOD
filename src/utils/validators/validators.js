a99957ee1e0a4844bdaea9df416a77e2cf080ecb
ShorSerj <shorser8@gmail.com>
2022-02-10 21:55:39 +0300

    delete excess files

--- src/utils/validators/validators.js
+++
@@ -1,9 +0,0 @@
-export const required = value => {
-    if (value) return undefined
-    return "Field is required"
-}
-
-export const phoneNumber = value =>
-value && !/^(0|[1-9][0-9]{9})$/i.test(value)
-  ? 'Invalid phone number, must be 10 digits'
-  : undefined
